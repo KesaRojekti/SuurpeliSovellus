878c51731b7ad0c48679633b741e91d95b736bef
Nekuin <Nekuin@gmail.com>
2018-07-18 22:34:55 +0300

    Added Firebase Crashlytics, some clean up on MainActivity

--- app/src/main/java/fi/samuliraty/suurpeli/suurpelifirebase/MainActivity.kt
+++ app/src/main/java/fi/samuliraty/suurpeli/suurpelifirebase/MainActivity.kt
@@ -1,25 +1,16 @@
 package fi.samuliraty.suurpeli.suurpelifirebase
 
-import android.app.NotificationManager
-import android.content.Context
+
 import android.content.Intent
 import android.support.design.widget.TabLayout
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
-import android.support.v4.view.ViewPager
 import android.os.Bundle
-import android.support.constraint.Placeholder
-import android.support.v4.app.NotificationManagerCompat
 import android.util.Log
-import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
@@ -27,7 +18,6 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
-import com.google.firebase.firestore.FirebaseFirestore
 
 import kotlinx.android.synthetic.main.activity_main.*
 
