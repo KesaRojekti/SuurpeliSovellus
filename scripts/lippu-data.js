90a8078e840d0398926e8cf66e767da9e3330854
Emil Toivainen <37114731+Em1lT@users.noreply.github.com>
2018-07-24 22:55:13 +0300

    Add files via upload

--- scripts/lippu-data.js
+++ scripts/lippu-data.js
@@ -3,8 +3,18 @@ class Lippu {
         this.latlng = latlng;
         this.active = active;
     }
+    
 }
-
+function checkAdmin(user){
+  var adminRef = firebase.database().ref("admin");
+  adminRef.once("value", function(snapshot){
+    snapshot.forEach(function(users){
+      if(user.uid == users.key){
+        displayTools();
+      }
+    });
+  });
+}
 //reference to "liput"
 var lippuRef = firebase.database().ref('liput');
 
@@ -53,32 +63,85 @@ function getLippuData(){
 }
 
 //add a flag
-function pushTest(){
-  var r1 = Math.floor(Math.random() * 100);
-  var r2 = Math.floor(Math.random() * 100);
-  var r = Math.random();
-  lippuarray.push(new Lippu(r1 + ", " + r2, false));
-  
+ var marker = [];
+ function pushTest(){
+   marker = new google.maps.Marker({position: start, map: map, draggable: true});
   writeLippuData(lippuarray);
 }
+ //Google maps
+    var map, i =0;
+    var start = {lat: 61.81555994553038, lng: 25.17069664313044};
+    function initMap() { 
+    map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 17,
+       center: start,
+      mapTypeId: `satellite`});
+      
+      map.addListener('click', function(e) {
 
+        if(marker.length == 1){
+          marker[0].setIcon(pinSymbol("#FF0"));
+        }
+        var location = e.latLng;
+        marker.push(new google.maps.Marker({position: location,
+           map: map,
+            draggable: true,
+          icon: pinSymbol("#FFF")}));
+      }); 
+      
+}
+//Symbol for marker icon
+function pinSymbol(color) {
+  return {
+      path: 'M 4,8 -1,-2 V -50 H 1 V -2 z M 1,-45 H 30 V -20 H 1 z',
+      fillColor: color,
+      fillOpacity: 1,
+      strokeColor: '#000',
+      strokeWeight: 2,
+      scale: 1,
+ };
+}
+
+
+    function UpdateFlags(){
+    
+      if(marker.length == 0){
+        var r1 = marker[1].getPosition().lat();
+      var r2 = marker[1].getPosition().lng();
+      }else{
+        var r1 = marker[i].getPosition().lat();
+        var r2 = marker[i].getPosition().lng();
+      }
+      i++;
+      console.log(marker.length, r1, r2);
+      lippuarray.push(new Lippu(r1 + ", " + r2, false));
+      writeLippuData(lippuarray);
+    }
 //remove a flag
 function removeFlag(){
   var index = parseInt(lippuarray.length-1);
   console.log("len before: " + lippuarray.length);
   lippuarray.splice(index, 1);
   console.log("len after: " + lippuarray.length);
+  
   writeLippuData(lippuarray);
 }
 
 function clearFlags(){
   lippuRef.remove();
+  for (var i = 0; i < marker.length; i++) {
+    marker[i].setMap(null);
 }
+marker.length = 1;
+console.log(marker.length);
+};
+
 
 //write all lippu objects into the database (from lippuarray)
 function writeLippuData(lippuarr){
   var i = 1;
   var updates = {};
+  
   lippuarr.forEach(function(l){
     var data = {
       LatLng: l.latlng,
@@ -103,6 +166,7 @@ function updateFlagData(){
     updates['lippu' + i] = data;
     i++;
   });
+
   //minimize writes by using update and not set
   firebase.database().ref('liput').update(updates);
 }
@@ -112,8 +176,10 @@ function activateNextFlag(){
   for(var i = 0; i < lippuarray.length; i++){
     if(lippuarray[i].active == false){
       console.log("found it at index: " + i);
+      marker[i].setIcon(pinSymbol("#0F0"));
+      marker[i + 1].setIcon(pinSymbol("#FF0"));
       lippuarray[i].active = true;
-      break;
+       break;
     }
   }
   updateFlagData();
@@ -126,6 +192,7 @@ function deactivateLastFlag(){
     if(lippuarray[i].active == false){
       console.log("found it at index: " + i);
       lippuarray[i-1].active = false;
+      marker[i].setIcon(pinSymbol("#FFF"));
       break;
     }
     //very last index if all were true
